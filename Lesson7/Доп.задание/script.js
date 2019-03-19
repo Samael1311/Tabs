c12e5d682df4e251a2ba7e71a57ac3a729203172
Rimma <teststudy1311@gmail.com>
2019-03-19 23:33:44 +0200

    Lesson 7.1

--- Lesson7/Доп.задание/script.js
+++ Lesson7/Доп.задание/script.js
@@ -3,23 +3,28 @@
 
 window.addEventListener('DOMContentLoaded', function(){
 
-let step = 0,
- r1 = document.getElementById('dandelion'),
- r2 = document.getElementById('dandelion2');
+	let btn = document.querySelector('#start');
+	btn.addEventListener('click', () => {
+		let step = 0,
+		r1 = document.getElementById('dandelion'),
+		r2 = document.getElementById('dandelion2');
+	 
+		function move(){
+			r1.style.left = step + 'px';
+			r2.style.top = step + 'px';
+			r2.style.left = step + 'px';
+	 
+		 if(step < window.screen.height-75){
+			 step += 2;
+				 requestAnimationFrame(move);
+		 }
+	 
+		}
+	 
+	 requestAnimationFrame(move);
+	})
 
- function move(){
-	 r1.style.left = step + 'px';
-	 r2.style.top = step + 'px';
-	 r2.style.left = step + 'px';
 
-	if(step < window.screen.height-75){
-		step += 2;
-			requestAnimationFrame(move);
-	}
-
- }
-
-requestAnimationFrame(move);
 
 
 
